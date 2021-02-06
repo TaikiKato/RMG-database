56356c54a2bd9fe44a3335ba68544450114f5e6b
davidfarinajr <farina.d@northeastern.edu>
2021-02-05 21:26:51 -0500

    added training reactions for surf_proton_electron_reduction_alpha_vdW

--- input/kinetics/families/Surface_Proton_Electron_Reduction_Alpha_vdW/training/reactions.py
+++ input/kinetics/families/Surface_Proton_Electron_Reduction_Alpha_vdW/training/reactions.py
@@ -8,21 +8,90 @@ Put kinetic parameters for specific reactions in this file to use as a
 training set for generating rate rules to populate this kinetics family.
 """
 
+# entry(
+#     index = 1,
+#     label = "CH3X + H + e <=> CH4X",
+#     degeneracy = 1,
+#     kinetics = SurfaceChargeTransfer(
+#         alpha = 0.12, # charge transfer coeff
+#         A = (2.5e14, 'm^3/(mol*s)'), # pre-exponential factor estimate 10^11 s^-1 * 2.5e5 m^2/mol / 1000 m^3/mol H+
+#         n = 0, # temperature coeff
+#         V0 = (0.51, 'V'), # reference potential
+#         Ea = (0.89, 'eV/molecule'), # activation energy
+#         Tmin = (200, 'K'),
+#         Tmax = (3000, 'K'),
+#         ne = -1, # electron stochiometric coeff 
+#     ),
+#     shortDesc = u"""https://doi.org/10.1016/j.cattod.2017.01.050""",
+#     longDesc = u"""
+# """,
+#     metal = "Pt",
+#     facet = "111",
+#     site = "",
+#     rank = 5,
+# )
+
 entry(
     index = 1,
     label = "CH3X + H + e <=> CH4X",
     degeneracy = 1,
     kinetics = SurfaceChargeTransfer(
-        alpha = 0.12, # charge transfer coeff
+        alpha = 0.23, # charge transfer coeff
         A = (2.5e14, 'm^3/(mol*s)'), # pre-exponential factor estimate 10^11 s^-1 * 2.5e5 m^2/mol / 1000 m^3/mol H+
         n = 0, # temperature coeff
-        V0 = (0.51, 'V'), # reference potential
-        Ea = (0.89, 'eV/molecule'), # activation energy
+        V0 = (-0.5, 'V'), # reference potential
+        Ea = (0.53, 'eV/molecule'), # activation energy
         Tmin = (200, 'K'),
         Tmax = (3000, 'K'),
         ne = -1, # electron stochiometric coeff 
     ),
-    shortDesc = u"""https://doi.org/10.1016/j.cattod.2017.01.050""",
+    shortDesc = u"""https://doi.org/10.1016/j.cattod.2018.03.048""",
+    longDesc = u"""
+""",
+    metal = "Pt",
+    facet = "111",
+    site = "",
+    rank = 5,
+)
+
+# entry(
+#     index = 2,
+#     label = "HOX + H + e <=> H2OX",
+#     degeneracy = 1,
+#     kinetics = SurfaceChargeTransfer(
+#         alpha = 0.07, # charge transfer coeff
+#         A = (2.5e14, 'm^3/(mol*s)'), # pre-exponential factor estimate 10^11 s^-1 * 2.5e5 m^2/mol / 1000 m^3/mol H+
+#         n = 0, # temperature coeff
+#         V0 = (0.39, 'V'), # reference potential
+#         Ea = (0.14, 'eV/molecule'), # activation energy
+#         Tmin = (200, 'K'),
+#         Tmax = (3000, 'K'),
+#         ne = -1, # electron stochiometric coeff 
+#     ),
+#     shortDesc = u"""https://doi.org/10.1016/j.cattod.2017.01.050""",
+#     longDesc = u"""
+# """,
+#     metal = "Pt",
+#     facet = "111",
+#     site = "",
+#     rank = 5,
+# )
+
+entry(
+    index = 2,
+    label = "HOX + H + e <=> H2OX",
+    degeneracy = 1,
+    kinetics = SurfaceChargeTransfer(
+        alpha = 0.11, # charge transfer coeff
+        A = (2.5e14, 'm^3/(mol*s)'), # pre-exponential factor estimate 10^11 s^-1 * 2.5e5 m^2/mol / 1000 m^3/mol H+
+        n = 0, # temperature coeff
+        V0 = (-0.5, 'V'), # reference potential
+        Ea = (0.12, 'eV/molecule'), # activation energy
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+        ne = -1, # electron stochiometric coeff 
+    ),
+    shortDesc = u"""https://doi.org/10.1016/j.cattod.2018.03.048""",
     longDesc = u"""
 """,
     metal = "Pt",
@@ -32,20 +101,20 @@ entry(
 )
 
 entry(
-    index = 2,
-    label = "HOX + H + e <=> H2OX",
+    index = 3,
+    label = "NH2X + H + e <=> NH3X",
     degeneracy = 1,
     kinetics = SurfaceChargeTransfer(
-        alpha = 0.07, # charge transfer coeff
+        alpha = 0.35, # charge transfer coeff
         A = (2.5e14, 'm^3/(mol*s)'), # pre-exponential factor estimate 10^11 s^-1 * 2.5e5 m^2/mol / 1000 m^3/mol H+
         n = 0, # temperature coeff
-        V0 = (0.39, 'V'), # reference potential
-        Ea = (0.14, 'eV/molecule'), # activation energy
+        V0 = (-0.5, 'V'), # reference potential
+        Ea = (0.23, 'eV/molecule'), # activation energy
         Tmin = (200, 'K'),
         Tmax = (3000, 'K'),
         ne = -1, # electron stochiometric coeff 
     ),
-    shortDesc = u"""https://doi.org/10.1016/j.cattod.2017.01.050""",
+    shortDesc = u"""https://doi.org/10.1016/j.cattod.2018.03.048""",
     longDesc = u"""
 """,
     metal = "Pt",
