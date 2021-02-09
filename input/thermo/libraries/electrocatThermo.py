5b0d06afb97f9e537189907c41c2c161e080289b
davidfarinajr <farina.d@northeastern.edu>
2021-02-08 21:27:11 -0500

    added to electrocatThermo library

--- input/thermo/libraries/electrocatThermo.py
+++ input/thermo/libraries/electrocatThermo.py
@@ -80,27 +80,85 @@ entry(
     facet = "111",
 )
 
+# entry(
+#     index = 4,
+#     label = "CO2X",
+#     molecule = 
+# """
+# 1 C u0 p0 {2,D} {3,D}
+# 2 O u0 p2 {1,D}
+# 3 O u0 p2 {1,D}
+# 4 X u0 p0
+# """,
+#     thermo = ThermoData(
+#         Tdata = ([300,400,500,600,800,1000,1500],'K'),
+#         Cpdata = ([8.869,9.845,10.626,11.264,12.229,12.898,13.822],'cal/(mol*K)'),
+#         H298 = (-98,'kcal/mol'),
+#         S298 = (28.54,'cal/(mol*K)'),
+#     ),
+#     shortDesc = u"""""",
+#     longDesc = 
+# u"""
+
+# """,
+#     metal = "Pt",
+#     facet = "111",
+# )
+
 entry(
-    index = 4,
-    label = "CO2X",
+    index = 5,
+    label = "H3O",
+    molecule =
+"""
+1 H u0 p0 c0 {3,S}
+2 H u0 p0 c0 {3,S}
+3 O u0 p2 c0 {1,S} {2,S}
+4 H u0 p0 c+1
+""",
+    thermo = NASA(
+        polynomials = [
+            NASAPolynomial(coeffs=[
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00,   0.000000000E+00,
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00], Tmin=(298.0,'K'), Tmax=(1000.0, 'K')),
+            NASAPolynomial(coeffs=[
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00,   0.000000000E+00,
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00], Tmin=(1000.0,'K'), Tmax=(3000.0, 'K')),
+        ],
+        Tmin = (298.0, 'K'),
+        Tmax = (3000.0, 'K'),
+    ),
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 6,
+    label = "O=CX-CX=O",
     molecule = 
 """
-1 C u0 p0 {2,D} {3,D}
-2 O u0 p2 {1,D}
-3 O u0 p2 {1,D}
-4 X u0 p0
+1 C u0 p0 c0 {2,S} {3,D} {5,S}
+2 C u0 p0 c0 {1,S} {4,D} {6,S}
+3 O u0 p2 c0 {1,D}
+4 O u0 p2 c0 {2,D}
+5 X u0 p0 c0 {1,S}
+6 X u0 p0 c0 {2,S}
 """,
     thermo = ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Cpdata = ([8.869,9.845,10.626,11.264,12.229,12.898,13.822],'cal/(mol*K)'),
-        H298 = (-100,'kcal/mol'),
-        S298 = (28.54,'cal/(mol*K)'),
+        Cpdata = ([15.24,17.52,19.03,20.13,21.6,22.53,23.69],'cal/(mol*K)'),
+        H298 = (-17.02,'kcal/mol'),
+        S298 = (49.17,'cal/(mol*K)'),
     ),
     shortDesc = u"""""",
     longDesc = 
 u"""
+OCCO from primary thermo library + Thermo group additivity estimation: adsorptionPt111(C-*R2C-*R2)
 
+instead of OCCO(S)
 """,
     metal = "Pt",
     facet = "111",
 )
+
+
+
