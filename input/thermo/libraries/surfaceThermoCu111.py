b53af3f636fb854d44798a90d68815ddd83ac26d
Chris B <56306881+ChrisBNEU@users.noreply.github.com>
2021-05-05 13:25:16 -0400

    correction: a5 had E-03 instead of E+03 in Cu111 library

--- input/thermo/libraries/surfaceThermoCu111.py
+++ input/thermo/libraries/surfaceThermoCu111.py
@@ -9,8 +9,8 @@ entry(
 """,
     thermo = NASA(
         polynomials = [
-            NASAPolynomial(coeffs=[3.78935111E+00, 1.10148021E-03,  -2.31320100E-06, 2.11937826E-09, -6.31350224E-13, -1.565287266E-03, -1.00616465E+01], Tmin=(298.0,'K'), Tmax=(1000.0, 'K')),
-            NASAPolynomial(coeffs=[4.06700165E+00, -5.01780079E-04,   6.70738856E-07, -1.79170942E-10, 8.86886631E-15, -1.589360806E-03, -1.12621699E+01], Tmin=(1000.0,'K'), Tmax=(2000.0, 'K')),
+            NASAPolynomial(coeffs=[3.78935111E+00, 1.10148021E-03,  -2.31320100E-06, 2.11937826E-09, -6.31350224E-13, -1.565287266E+03, -1.00616465E+01], Tmin=(298.0,'K'), Tmax=(1000.0, 'K')),
+            NASAPolynomial(coeffs=[4.06700165E+00, -5.01780079E-04,   6.70738856E-07, -1.79170942E-10, 8.86886631E-15, -1.589360806E+03, -1.12621699E+01], Tmin=(1000.0,'K'), Tmax=(2000.0, 'K')),
         ],
         Tmin = (298.0, 'K'),
         Tmax = (2000.0, 'K'),
@@ -18,10 +18,10 @@ entry(
     longDesc = u"""Using the Pt111 value for NASA polynomial, but adjusting the a5 parameter by (delta Binding Energy)/R.
             DFT binding energy on Pt111: -0.054 eV.
             DFT binfing energy on Cu111: -0.08 eV. 
-            delta BE: -0.054 - (-0.08) = 0.026 eV = 0.026 eV * (1.602176565e-19 J/eV) * (6.02214*10^23) = 2508 J/mol
+            delta BE: -0.054 - (-0.08) = +0.026 eV = 0.026 eV * (1.602176565e-19 J/eV) * (6.02214*10^23) = 2508 J/mol
             
-            a5_low = -1.86700333*10^3 + (2508 J/mol) / (8.3145 J/mol K) = -1.565287266E-03
-            a5_high = -1.89107687*10^3 + (2508 J/mol) / (8.3145 J/mol K) = -1.589360806E-03
+            a5_low = -1.86700333*10^3 + (2508 J/mol) / (8.3145 J/mol K) = -1.565287266E+03
+            a5_high = -1.89107687*10^3 + (2508 J/mol) / (8.3145 J/mol K) = -1.589360806E+03
             
             Source for H2 Binding Energy: 
             "On the hydrogen adsorption and dissociation on Cu surfaces and nanorows"
